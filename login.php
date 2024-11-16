33ad044ae0449e69fb69630f1d2eddca271dba14
durga1223prasad <154881830+durga1223prasad@users.noreply.github.com>
2024-11-16 08:16:04 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<meta charset="utf-8"> 
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
+<title>Login</title> 
+<style type="text/css"> 
+*{ 
+} 
+body{ 
+margin: 0; 
+padding: 0; 
+box-sizing: border-box; 
+font-family: "sans-serif"; 
+display: flex; 
+   justify-content: center; 
+   align-items: center; 
+   min-height: 100vh; 
+   background: darksalmon; 
+  } 
+ </style> 
+</head> 
+<body> 
+ <form method="post" action="validate.php"> 
+ <table> 
+  <tr> 
+   <td colspan=2> 
+    <input class="input_box" type="textbox" name="userName" 
+placeholder="User ID" id="userName_ID"> 
+   </td> 
+  </tr> 
+  <tr> 
+   <td colspan="2"> 
+    <input type="Password" name="password" placeholder="Password" 
+id="password_ID" class="input_box"> 
+   </td> 
+  </tr> 
+  <tr> 
+   <td> 
+    <input type="checkbox" name="remember" value="1"> 
+   </td> 
+   <td> 
+    Remember Me 
+   </td> 
+  </tr> 
+  <tr>  
+<td colspan="2"> 
+<input type="submit" value="login" name="login"> 
+</td> 
+</tr> 
+</table> 
+</form> 
+</body> 
+</html> 
+<?php 
+?> 
+ 
